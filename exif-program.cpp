79a0374ff84f733b7f3eea1f7b9f50c0fb170e17
Gregory Keenan <gfkeenan4@gmail.com>
2022-04-11 15:18:05 +0100

    Update for refactor

--- exif-program.cpp
+++ exif-program.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include "Headers/additionalMethods.hpp"
 
+//written for MacOS
 #include <unistd.h>
 
 //Written by Gregory Keenan to run a simple program to set the exif data for pictures
@@ -21,7 +23,7 @@ void writeExifDatatoAll(std::string *camera, std::string *model, std::string *le
 	// exiftool -Make="Olympus" -Model="µ[mju]-i" -Lens="35mm" *.jpg
 	//This will write the inputed details to all files in the folder
 	
-	std::cout << "Camera Data Passed " << *camera << "\n";
+	//std::cout << "Camera Data Passed " << *camera << "\n";
 	std::string CombinedOutput;
 	std::string CameraArg, ModelArg, LensArg;
 
@@ -37,6 +39,7 @@ void writeExifDatatoAll(std::string *camera, std::string *model, std::string *le
 	//std::system(CombinedOutput.c_str());
 
 	execl("/usr/local/bin/exiftool", "exiftool", CameraArg.c_str(), ModelArg.c_str(), LensArg.c_str(), "./", NULL);
+	
 
 }
 
@@ -56,12 +59,32 @@ void inputDetails()
 	std::cout << "Please enter the Lens used: ";
 	std::cin >> lens;
 
-	std::cout << "You have entered:" << "\n" << "Camera: " << camera << " Model: " << model << " Lens: " << lens << "\n";
+/* 		--MAC OS Specific--
+Foreground colour: "\x1b[30m"
+Background colour: "\x1b[40m"
+Foreground and background: "\x1b[30:40m"
+Reset all colour back to normal: "\x1b[0m"
++---------+------------+------------+
+|  color  | foreground | background |
+|         |    code    |    code    |
++---------+------------+------------+
+| black   |     30     |     40     |
+| red     |     31     |     41     |
+| green   |     32     |     42     |
+| yellow  |     33     |     43     |
+| blue    |     34     |     44     |
+| magenta |     35     |     45     |
+| cyan    |     36     |     46     |
+| white   |     37     |     47     |
++---------+------------+------------+
+*/
+	std::cout << "\x1b[31m" << "You have entered: " << "\n" << "\x1b[47m" << "Camera: " << camera << " Model: " << model << " Lens: " << lens << " \x1b[0m \n";
 
+	//std::cout << "\x1b[0m"; //reset text colour to normal
 	std::cout << "Is this correct? (Y/N)" << "\n";
 
 	std::cin >> yn;
-
+	
 	if(!yn.compare("Y") || !yn.compare("y"))
 	{
 		std::cout << "Yes\n";
@@ -76,7 +99,7 @@ void inputDetails()
 
 	else
 	{
-		std::cout << "Please use correct formating\n";
+		std::cout << "Please use correct formating: \"Y/y/N/n\" \n";
 		inputDetails();
 	}
 
@@ -85,7 +108,21 @@ void inputDetails()
 int main()
 {
 	introText();
-	inputDetails();
+
+	std::string input;
+	std::cout << "What do you want to do? \n" << "1) Update 35mm Exif data \n" << "2) NUKE/Remove all Exif data \n" << "Please enter your choice: ";
+	std::cin >> input;
+
+	if(!input.compare("1"))
+	{
+		inputDetails();
+	} 
+	else if (!input.compare("2"))
+	{
+		totalWipeExifData();
+	} else {
+		std::cout << "Invalid choice - exiting program" << std::endl;
+	}
 
 	return 0;
 }
