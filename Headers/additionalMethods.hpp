bd7832da983364e36f78c01e8da8a82d02d7cd1b
Gregory Keenan <gfkeenan4@gmail.com>
2022-04-11 15:16:54 +0100

    Adding a Exif wiping method

---
+++ Headers/additionalMethods.hpp
@@ -0,0 +1,4 @@
+#pragma once
+//Header file for testing an idea
+
+void totalWipeExifData();
---
+++ wipe.cpp
@@ -0,0 +1,10 @@
+//Wiping Exif data module
+//Written by Gregory Keenan
+#include <iostream>
+#include <string>
+#include "Headers/additionalMethods.hpp"
+
+void totalWipeExifData()
+{
+	std::cout << "Total wiping of data";
+}
